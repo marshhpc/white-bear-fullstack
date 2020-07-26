a135bb2ffc8d3cbb9af0a4352643d0292a2edba8
marshhpc <64800792+marshhpc@users.noreply.github.com>
2020-07-25 18:40:01 -0700

    prepare for deploy

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 // var cors = require("cors");
 
 // app.use(cors());
 
 app.use("/api/v1/users", require("./api/v1/users"));
 app.use("/api/v1/memory-cards", require("./api/v1/memory-cards"));
-app.get("/", (req, res) => res.send("Hello World!"));
+
+app.use(express.static("client/build"));
+app.get("*", (req, res) => {
+   res.sendFile(path.resolve(__dirname, "client", "build", "index.html")); // ./client/build/index.html
+});
 
 const port = process.env.PORT || 3045;
 app.listen(port, () =>
