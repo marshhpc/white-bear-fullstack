cb47ae99b3d03c6dbe5ff87cd5e2f8acd4440a7b
marshhpc <64800792+marshhpc@users.noreply.github.com>
2020-07-25 19:09:35 -0700

    push to deploy

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-// var cors = require("cors");
-
-// app.use(cors());
 
 app.use("/api/v1/users", require("./api/v1/users"));
 app.use("/api/v1/memory-cards", require("./api/v1/memory-cards"));
 
 app.use(express.static("client/build"));
 app.get("*", (req, res) => {
-   res.sendFile(path.resolve(__dirname, "client", "build", "index.html")); // ./client/build/index.html
+   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
 const port = process.env.PORT || 3045;
